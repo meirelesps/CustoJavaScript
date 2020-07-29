fd0e6c7c1e1d3b595b1c76848911b9c5e7404d28
meirelesps <68961425+meirelesps@users.noreply.github.com>
2020-07-29 12:46:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let nome = 'Rafa';
+let idade = 25;
+let estaAprovado = true;
+let sobrenome = undefined;
+
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'de souza'
+};
+
+console.log(pessoa);
+let familia = [26,45,0,17];
+console.log(familia.length);
+console.log(familia[0]);
+
+let corSite = 'azul';
+function resetacor(cor){
+    corSite = cor;
+}
+
+console.log(corSite);
+resetacor('vermelho');
+console.log(corSite);
+
